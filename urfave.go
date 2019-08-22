2b08ed1fddbf53750899e34ec8bf8180649f0081
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-08-22 11:58:15 +0900

    feat: add the function WrapAction

--- urfave.go
+++ urfave.go
@@ -9,5 +9,14 @@ func ConvErrToExitError(err error) error {
 	if err == nil {
 		return nil
 	}
+	if _, ok := err.(*cli.ExitError); ok {
+		return err
+	}
 	return cli.NewExitError(err.Error(), 1)
+}
+
+func WrapAction(f func(c *cli.Context) error) func(c *cli.Context) error {
+	return func(c *cli.Context) error {
+		return ConvErrToExitError(f(c))
+	}
 }
