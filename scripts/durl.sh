2aea6c91d754c535a7e412e344c62d489265a9ad
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2019-08-25 08:23:01 +0900

    build: replace npm script to cmdx
    https://github.com/suzuki-shunsuke/cmdx

--- scripts/durl.sh
+++ scripts/durl.sh
@@ -1,7 +1,4 @@
 find . \
-  -type d -name node_modules -prune -o \
   -type d -name .git -prune -o \
-  -type d -name dist -prune -o \
   -type f -print | \
-  grep -v package-lock.json | \
-  durl check || exit 1
+  durl check
